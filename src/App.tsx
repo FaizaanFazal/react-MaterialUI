e44bd6a2f97f7cbd9ee5290d5727414417d52c69
Faizaan <dkfaizaan12@gmail.com>
2023-11-29 20:32:50 +0500

    Radio buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { MuiButton } from './components/MuiButton';
+import { MuiRadioButton } from './components/MuiRadioButton';
 import { MuiSelect } from './components/MuiSelect';
 import { MuiTextField } from './components/MuiTextField';
 import { MuiTypography } from './components/MuiTypography';
@@ -16,7 +17,7 @@ function App() {
       <Divider variant="middle" sx={{ mt: 3,mb:3 }}/>
       <MuiSelect />
       <Divider variant="middle" sx={{ mt: 3,mb:3 }}/>
-      
+      <MuiRadioButton />
     </div>
   );
 }
